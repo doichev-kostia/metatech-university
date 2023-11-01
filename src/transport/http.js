19f4cf7d68e11e1ac35c03753c1d6773a2f8d765
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-11-01 13:22:16 +0100

    fix: arguments and CORS

--- src/transport/http.js
+++ src/transport/http.js
@@ -1,31 +1,44 @@
-'use strict';
+"use strict";
 
-const http = require('node:http');
+const http = require("node:http");
 
-const receiveArgs = async (req) => {
-  const buffers = [];
-  for await (const chunk of req) buffers.push(chunk);
-  const data = Buffer.concat(buffers).toString();
-  return JSON.parse(data);
+const parseBody = async (req) => {
+    const buffers = [];
+    for await (const chunk of req) buffers.push(chunk);
+    const data = Buffer.concat(buffers).toString();
+    return JSON.parse(data);
 };
 
 module.exports = (routing, port) => {
-  http.createServer(async (req, res) => {
-    const { url, socket } = req;
-    const [name, method, id] = url.substring(1).split('/');
-    const entity = routing[name];
-    if (!entity) return void res.end('Not found');
-    const handler = entity[method];
-    if (!handler) return void res.end('Not found');
-    const src = handler.toString();
-    const signature = src.substring(0, src.indexOf(')'));
-    const args = [];
-    if (signature.includes('(id')) args.push(id);
-    if (signature.includes('{')) args.push(await receiveArgs(req));
-    console.log(`${socket.remoteAddress} ${method} ${url}`);
-    const result = await handler(...args);
-    res.end(JSON.stringify(result.rows));
-  }).listen(port);
+    http.createServer(async (req, res) => {
+        const { url, socket } = req;
+        const [name, method] = url.substring(1).split("/");
+        const entity = routing[name];
+        if (!entity) return void res.end("Not found");
+        const handler = entity[method];
+        if (!handler) return void res.end("Not found");
+        if (req.method === "OPTIONS") {
+            res.writeHead(200, {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+            });
+            return void res.end(http.STATUS_CODES[200]);
+        } else if (req.method !== "POST") {
+            res.writeHead(405)
+            return void res.end(http.STATUS_CODES[405]);
+        }
+        const data = await parseBody(req);
+        console.log(`${socket.remoteAddress} ${method} ${url}`);
+        const result = await handler(data);
+        res.writeHead(200, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Content-Type": "application/json; charset=UTF-8",
+        });
+        res.end(JSON.stringify(result.rows));
+    }).listen(port);
 
-  console.log(`API on port ${port}`);
+    console.log(`API on port ${port}`);
 };
