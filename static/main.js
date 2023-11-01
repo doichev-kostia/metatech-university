7900807fa1efc06332a351984cdf58f7610121a2
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-11-01 13:55:06 +0100

    feat: integrated UI with api client

--- static/main.js
+++ static/main.js
@@ -1,9 +1,19 @@
 import { client } from "./client/api.js";
 
-const button = document.querySelector("#btn");
+const form = document.querySelector("#get-user");
+const output = document.querySelector("#output");
 
-button.addEventListener("click", async () => {
-   const response = await client.api.user.read({ id: 3 });
-   const data = await response.json();
-   console.dir({ data })
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const id = formData.get("id");
+    const response = await client.api.user.read({id});
+    const data = await response.json();
+    output.innerHTML = toHTML(data);
 });
+
+function toHTML(struct) {
+    return `
+    <pre>${JSON.stringify(struct, null, 2)}</pre>
+    `
+}
